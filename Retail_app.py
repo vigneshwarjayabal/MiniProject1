ce4e60bffd769eb180e9d5fb6b0b0bc2bfc1c5b7
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 13:47:33 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -1,22 +1,3 @@
 import streamlit as st
-import psycopg2
-import pandas as pd
 
-# Connect to PostgreSQL
-def connect_to_db():
-    connection = psycopg2.connect(
-        host="your-aws-host",
-        database="your-database-name",
-        user="your-username",
-        password="your-password",
-        port="your-port"
-    )
-    return connection
-
-# Fetch data
-def fetch_data(query):
-    conn = connect_to_db()
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
-
+st.title("Simple Streamlit App")
