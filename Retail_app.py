bd437504bb76cc99fe0f4cc7dd0f6fd271d159e3
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 13:48:43 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Simple Streamlit App")
+
+user_input = st.text_input("Enter your name:")
+
+# Display a message when the user enters their name
+if user_input:
+    st.write(f"Hello, {user_input}!")
+else:
+    st.write("Please enter your name above.")
