a963ab76af26792d5fd953ba67e577fb271f213e
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 12:37:51 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -4,3 +4,14 @@ import psycopg2
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+
+def create_connection():
+    conn = psycopg2.connect(
+        host = 'miniprojectdb.cv8yqowiopcb.ap-south-1.rds.amazonaws.com',
+        port = '5432',
+        database = 'postgres',
+        user = 'postgres',
+        password = 'dhoni00728'
+
+    )
+    return conn
