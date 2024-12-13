1bb081e8e8208d8b618f20b09068f4c1d2e1cdfc
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 14:34:00 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -4,6 +4,4 @@ conn = st.connection("postgresql", type="sql")
 
 df = conn.query('SELECT * FROM orders;', ttl="10m")
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+print(df)
