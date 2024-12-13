a86856f22d8dfc116250cdfce49b232fd896052e
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 21:55:24 +0530

    Update Retail_app.py

--- Retail_app.py
+++ Retail_app.py
@@ -1,34 +1,17 @@
-import streamlit as st
+import psycopg2
 import pandas as pd
-from db_config import get_db_connection
-from queries import queries
-import matplotlib.pyplot as plt
-import seaborn as sns
+import streamlit as st
 
-# App Title
-st.title("Retail Order Data Analysis")
-st.sidebar.header("Business Insights")
 
-# Sidebar for Query Selection
-selected_query = st.sidebar.selectbox("Select an Insight", list(queries.keys()))
-
-# Execute Query
-def execute_query(query):
-    with get_db_connection() as conn:
-        return pd.read_sql(query, conn)
-
-# Display Query Result
-if selected_query:
-    st.subheader(selected_query)
-    query = queries[selected_query]
+def database_connection():
     try:
-        data = execute_query(query)
-        st.write(data)
-
-        # Visualization
-        if not data.empty:
-            st.bar_chart(data.set_index(data.columns[0]))
-            st.line_chart(data.set_index(data.columns[0]))
-
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+        connection = psycopg2.connect(
+            host=st.secrets["postgresql"]["host"],  
+            user=st.secrets["postgresql"]["user"],  
+            password=st.secrets["postgresql"]["password"], 
+            database=st.secrets["postgresql"]["database"]  
+        )
+        return connection
+    except psycopg2.Error as err:
+        st.error(f"Error connecting to database: {err}")
+        return None
