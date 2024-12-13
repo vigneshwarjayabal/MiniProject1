38c9d4f3f5d5c8b1133b3ba71ab1559f3aff1a14
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 13:37:22 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
-from sqlalchemy import create_engine
-host = 'miniprojectdb.cv8yqowiopcb.ap-south-1.rds.amazonaws.com'
-port = '5432'
-database = 'postgres'
-user = 'postgres'
-password = 'dhoni00728'
 
-connection_string = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
-engine = create_engine(connection_string)
+# Set the title of the app
+st.title("Simple Streamlit App")
+
+# Create a text input field for user input
+user_input = st.text_input("Enter your name:")
+
+# Display a message when the user enters their name
+if user_input:
+    st.write(f"Hello, {user_input}!")
+else:
+    st.write("Please enter your name above.")
