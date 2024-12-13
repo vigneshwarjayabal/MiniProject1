19b00e7b6d27d2d19c998855d1e86bac7758f3bf
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 22:12:44 +0530

    Update Retail_app.py

--- Retail_app.py
+++ Retail_app.py
@@ -1,6 +1,8 @@
 import psycopg2
 import pandas as pd
 import streamlit as st
+from queries import business_insights, provided_query, my_query 
+
 
 
 def database_connection():
@@ -16,6 +18,36 @@ def database_connection():
         st.error(f"Error connecting to database: {err}")
         return None
 
+def execute_query(query):
+    try:
+        conn = database_connection()
+        if conn is None:
+            return pd.DataFrame()  # If connection failed, return an empty DataFrame
+        cursor = conn.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        column_names = [desc[0] for desc in cursor.description]
+        conn.close()  # Close the connection after execution
+        return pd.DataFrame(result, columns=column_names)
+    except Exception as e:
+        st.error(f"Error executing the query: {e}")
+        return pd.DataFrame()
+
+def display_results(selection, query_dict):
+    st.subheader(selection)
+    selected_query = st.selectbox("Choose a query", list(query_dict.keys()))
+    st.write("**Query:**")
+    st.code(f"{query_dict[selected_query]}")
+    if st.button("Run", key=f"run_button_{selected_query}"):
+        result = execute_query(query_dict[selected_query])
+        if not result.empty:
+            st.write("**Result:**")
+            return st.dataframe(result)
+        else:
+            st.warning("No data returned for the query.")
+
+
+
 
 
 st.title("Retail Sales Analysis")
@@ -23,8 +55,13 @@ st.title("SQL Querry App")
 st.sidebar.title("Menu")
 option=st.sidebar.radio("Choose an option",["Provided Queries", "Own Queries", "Create AI-Powered Custom Query"])
 
-if option == "Provided Queries":
-    display_results("Provided Queries", Provided_Queries)
+if option == "business_insights":
+    display_results("business_insights", business_insights)
 
-elif option == "Own Queries":
-    display_results("Own Queries", Own_Queries)
+elif option == "provided_query":
+    display_results("provided_query", provided_query)
+elif option == "my_query":
+    display_results("my_query", my_query)
+else:
+    st.write("Please select a valid option")
+    
