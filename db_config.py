0b613b573b138b237900ebcdf3c9befff2d4c6ae
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 20:27:49 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,1 +1,7 @@
+from sqlalchemy import create_engine
 
+def get_db_connection():
+    engine = create_engine(
+        "postgresql+psycopg2://postgres:dhoni00728@miniprojectdb.cv8yqowiopcb.ap-south-1.rds.amazonaws.com:5432/postgres"
+    )
+    return engine.connect()
