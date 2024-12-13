bc665729842638b8b61f13ef7ba90ccc49117ca2
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 14:30:14 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 conn = st.connection("postgresql", type="sql")
+
+df = conn.query('SELECT * FROM orders;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
