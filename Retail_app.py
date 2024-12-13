75699495df6e124b22ea967f0f4f96298c25c09c
vigneshwar j <vigneshwarjayabal@gmail.com>
2024-12-13 13:43:05 +0000

    update

--- Retail_app.py
+++ Retail_app.py
@@ -1,13 +1,22 @@
 import streamlit as st
+import psycopg2
+import pandas as pd
 
-# Set the title of the app
-st.title("Simple Streamlit App")
+# Connect to PostgreSQL
+def connect_to_db():
+    connection = psycopg2.connect(
+        host="your-aws-host",
+        database="your-database-name",
+        user="your-username",
+        password="your-password",
+        port="your-port"
+    )
+    return connection
 
-# Create a text input field for user input
-user_input = st.text_input("Enter your name:")
+# Fetch data
+def fetch_data(query):
+    conn = connect_to_db()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
 
-# Display a message when the user enters their name
-if user_input:
-    st.write(f"Hello, {user_input}!")
-else:
-    st.write("Please enter your name above.")
